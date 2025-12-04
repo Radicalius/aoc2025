78d85477746e6eea9396f7ec77bca8ad1c55d5e8
Radicalius <mr.zacharycotton@gmail.com>
2025-12-03 19:07:40 -0800

    Imlement Solution Loader

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::{env, fs};
+
+trait Solution {
+    fn part1(&self, input: &str) -> i64;
+    fn part2(&self, input: &str) -> i64;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().collect::<Vec<String>>();
+    
+    let dayStr = match args.get(1) {
+        Some(x) => x,
+        None => panic!("Please provide the aoc day to run")
+    };
+
+    let day = match dayStr.parse::<usize>() {
+        Ok(x) => x,
+        Err(e) => panic!("error parsing aoc day argument")
+    };
+
+    assert!(day > 0 && day < 25, "day must be in [1, 25]");
+
+    let solutions: Vec<Box<dyn Solution>> = vec![
+
+    ];
+
+    let filename = String::from("inputs/")+dayStr+".txt";
+    let input = fs::read_to_string(filename).expect("error opening input file");
+    
+    let solution: &Box<dyn Solution>  = match solutions.get(day - 1) {
+        Some(x) => x,
+        None => panic!("solution for day not yet implemented")
+    };
+
+    println!("part 1: {}", solution.part1(&input));
+    println!("part 2: {}", solution.part2(&input));
 }
