b7fbd113220060ad99bc97d61f50e27e3ba4f9c7
Radicalius <mr.zacharycotton@gmail.com>
2025-12-04 20:47:52 -0800

    Day 3 Checkpoint

--- src/day3.rs
+++ src/day3.rs
@@ -1,19 +1,21 @@
+use std::collections::HashMap;
+
 use crate::solution::Solution;
 
 pub struct Day3Solution {}
 
 impl Day3Solution {
-  fn parse(&self, inp: &str) -> Vec<Vec<u32>> {
-    let mut res: Vec<Vec<u32>> = vec![];
+  fn parse(&self, inp: &str) -> Vec<Vec<i64>> {
+    let mut res: Vec<Vec<i64>> = vec![];
 
     for line in inp.split("\n") {
       if line == "" {
         continue;
       }
 
-      let mut bank: Vec<u32> = vec![];
+      let mut bank: Vec<i64> = vec![];
       for battery in line.chars().collect::<Vec<char>>() {
-        let joltage = match battery.to_string().parse::<u32>() {
+        let joltage = match battery.to_string().parse::<i64>() {
           Ok(x) => x,
           Err(e) => panic!("invalid joltage: {} {}", battery, e)
         };
@@ -27,7 +29,7 @@ impl Day3Solution {
     return res;
   }
 
-  fn find_max_joltage(&self, bank: Vec<u32>) -> u32 {
+  fn find_max_joltage_2_battery(&self, bank: &[i64]) -> i64 {
     let mut cur_max = 0;
     let mut best_bat = 0;
 
@@ -44,19 +46,60 @@ impl Day3Solution {
 
     return cur_max;
   }
+
+  fn find_max_joltage_n_batteries(&self, bank: &[i64], n: usize, cache: &mut HashMap<(usize, usize), i64>) -> i64 {
+    // let cache_key = (bank.len(), n);
+    // if cache.contains_key(&cache_key) {
+    //   return *cache.get(&cache_key).unwrap();
+    // }
+    
+    if n == 0 {
+      return 0;
+    }
+
+    if bank.len() == 1 {
+      return bank[0];
+    }
+    
+    if bank.len() <= n {
+      return bank[0] * 10_i64.pow((n - 1) as u32) + self.find_max_joltage_n_batteries(&bank[1..], n-1, cache);
+    }
+
+    let use_first = bank[0] * 10_i64.pow((n - 1) as u32) + self.find_max_joltage_n_batteries(&bank[1..], n-1, cache);
+    let not_use_first = self.find_max_joltage_n_batteries(&bank[1..], n, cache);
+
+    if use_first > not_use_first {
+      //cache.insert(cache_key, use_first);
+      return use_first;
+    }
+
+    //cache.insert(cache_key, not_use_first);
+    return not_use_first;
+  }
+
+  fn sum_joltage<JoltageFinder>(&self, inp: Vec<Vec<i64>>, mut find_joltage: JoltageFinder) -> i64 
+  where JoltageFinder: FnMut(&[i64]) -> i64 {
+    let mut joltage = 0;    
+    for bank in inp {
+      joltage += find_joltage(&bank)
+    }
+
+    return joltage;
+  }
+
 }
 
 impl Solution for Day3Solution {
   fn part1(&self, input: &str) -> i64 {
-    let mut joltage = 0;    
-    for bank in self.parse(input) {
-      joltage += self.find_max_joltage(bank)
-    }
-
-    return joltage as i64;
+    return self.sum_joltage(
+      self.parse(input),
+      |bank| self.find_max_joltage_2_battery(bank))
   }
 
   fn part2(&self, input: &str) -> i64 {
-    return 0;
+    let mut cache: HashMap<(usize, usize), i64> = HashMap::new();
+    return self.sum_joltage(
+      self.parse(input),
+      |bank| self.find_max_joltage_n_batteries(bank, 12, &mut cache))
   }
 }
